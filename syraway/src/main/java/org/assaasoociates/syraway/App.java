da84763d5354b8ee717eaabf948aa46e10a1958b
waheb <r.waheb@gmail.com>
2010-12-08 18:38:01 +0100

    Create the maven parent Project

---
+++ syraway/src/main/java/org/assaasoociates/syraway/App.java
@@ -0,0 +1,13 @@
+package org.assaasoociates.syraway;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

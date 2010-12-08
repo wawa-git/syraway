cf88dfb8fdc7859c86efe1861d4fe6a76be3e290
waheb <r.waheb@gmail.com>
2010-12-08 19:33:50 +0100

    Create the Maven Module This module include the model data

---
+++ syraway/model/src/main/java/com/assaassociates/syraway/model/App.java
@@ -0,0 +1,13 @@
+package com.assaassociates.syraway.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
